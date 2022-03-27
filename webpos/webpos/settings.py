76584c500a7aada34a57e30b10ad793fc059d88f
stevescilar <stevemuambi@gmail.com>
2022-03-27 06:47:36 +0300

    setting up

--- webpos/webpos/settings.py
+++ webpos/webpos/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-d=dpoza1-2u2z8b23+$id+v%%0s^q&bb0)jx&gk#dx*%*_@$j_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'dashboard',
 ]
 
 MIDDLEWARE = [
