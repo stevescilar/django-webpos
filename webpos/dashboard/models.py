2d8c7210cfc736d52de92bba17161e80fdb404a1
stevescilar <stevemuambi@gmail.com>
2022-03-30 11:03:53 +0300

    winding up

--- webpos/dashboard/models.py
+++ webpos/dashboard/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+import uuid
+
+class productCategory(models.Model):
+    category_name = models.CharField(max_length=200)
+    Description = models.CharField(max_length=200)
+    created = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.category_name
+    
+
+    
