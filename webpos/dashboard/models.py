b55432d87acb0842c644a0e1f45dedc783bcd6bc
stevescilar <stevemuambi@gmail.com>
2022-03-30 11:14:23 +0300

    winding up

--- webpos/dashboard/models.py
+++ webpos/dashboard/models.py
@@ -14,7 +14,11 @@ class Item(models.Model):
     item_name = models.CharField(max_length=20)
     item_desc = models.CharField(max_length=200)
     item_price = models.FloatField(verbose_name='price')
-    item_cat = models.ForeignKey(productCategory,verbose_name='category')
+    item_cat = models.ForeignKey(productCategory,verbose_name='category',on_delete=models.CASCADE)
+
+
+    def __str__ (self):
+        return self.item_name
     
 
     
