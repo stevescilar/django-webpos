ede35839d5c0181217fab3c62405dd7587f7e810
stevescilar <stevemuambi@gmail.com>
2022-04-12 11:23:39 +0300

    updated

--- webpos/webpos/settings.py
+++ webpos/webpos/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'dashboard.apps.DashboardConfig',
+    'category',
 ]
 
 MIDDLEWARE = [
@@ -120,7 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = BASE_DIR /'static'
+STATICFILES_DIRS = [
+    'webpos/static',
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
