b73e539f2ebf9b7fcae0a738fc2e38933216c1e9
stevescilar <stevemuambi@gmail.com>
2022-04-13 10:02:19 +0300

    updated

--- webpos/webpos/urls.py
+++ webpos/webpos/urls.py
@@ -21,5 +21,5 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home, name='home'),
-    # path('',include('dashboard.urls')),
+    path('store/',include('store.urls')),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
