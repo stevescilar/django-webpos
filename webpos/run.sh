7819a7e29d5cbc0485340bc8fd18e3356ba8c62e
stevescilar <stevemuambi@gmail.com>
2022-04-14 17:40:52 +0300

    updated

---
+++ webpos/run.sh
@@ -0,0 +1,7 @@
+#bashscript to run server on custom port
+exec  ./manage.py runserver 127.0.0.1:2022
+# exec ./manage.py runserver 0.0.0.0:2000
+
+# linuc command
+# source /home/samurai/.bashrc && runserver
+
